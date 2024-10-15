419c388a212d7fe38726e3fb5a48a35804b0cdc4
SakamotoKai <221093@st.yoshida-g.ac.jp>
2024-10-15 09:30:33 +0900

    マップシステム

--- 00_base_Sakamoto/source/MapSystem.h
+++ 00_base_Sakamoto/source/MapSystem.h
@@ -1,39 +1,30 @@
-////========================================================================================
-////
-//// マップシステム[MapSystem.h]
-//// Author: 坂本　翔唯
-////
-////========================================================================================
-//#ifndef _FADE_H_
-//#define _FADE_H_
+//========================================================================================
 //
-//#include "main.h"
-//#include "manager.h"
+// マップシステム[MapSystem.h]
+// Author: 坂本　翔唯
 //
-//class CObject2D;
-//
-////フェードのクラス宣言
-//class CFade
-//{
-//public:
-//	//フェードの状態
-//	typedef enum
-//	{
-//		FADE_NONE = 0,	//何もしていない状態
-//		FADE_IN,		//フェードイン状態
-//		FADE_OUT,		//フェードイン状態
-//		FADE_MAX
-//	}FADE;
-//
-//	CFade();
-//	~CFade();
-//
-//	void Init(CScene::MODE modeNext);
-//	void Uninit();
-//	void Update();
-//	void Draw(void);
-//
-//protected:
-//};
-//
-//#endif
+//========================================================================================
+#ifndef _MAPSYSTEM_H_
+#define _MAPSYSTEM_H_
+
+#include "main.h"
+#include "manager.h"
+
+class CObject2D;
+
+//フェードのクラス宣言
+class CMapSystem
+{
+public:
+	CMapSystem();
+	~CMapSystem();
+
+	void Init();
+	void Uninit();
+	void Update();
+	void Draw();
+
+protected:
+};
+
+#endif
