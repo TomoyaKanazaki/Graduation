3d9a753e96b0aec37f813fe9b3cf000648aec191
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2024-10-16 10:27:55 +0900

    itemの親クラス追加

--- 00_base_Sakamoto/source/cross.h
+++ 00_base_Sakamoto/source/cross.h
@@ -8,13 +8,13 @@
 #define _CROSS_H_
 
 #include "main.h"
-#include "objectX.h"
+#include "item.h"
 
 //前方宣言
 class CObjGauge2D;
 
 //オブジェクトプレイヤークラス
-class CCross : public CObjectX
+class CCross : public CItem
 {
 public:
 
