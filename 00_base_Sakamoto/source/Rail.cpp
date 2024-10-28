d7146f193628a4d5771e9146cadfe2f709312aa3
SakamotoKai <221093@st.yoshida-g.ac.jp>
2024-10-28 12:01:50 +0900

    傾きで動くブロックと動きを表すレールの基盤を作成

---
+++ 00_base_Sakamoto/source/Rail.cpp
@@ -0,0 +1,194 @@
+//============================================
+//
+//	レールの処理 [Rail.cpp]
+//	Author:sakamoto kai
+//
+//============================================
+#include "Rail.h"
+#include "renderer.h"
+#include "manager.h"
+#include "texture.h"
+#include "XModel.h"
+
+//==========================================
+//  定数定義
+//==========================================
+namespace
+{
+	const D3DXVECTOR3 SAMPLE_SIZE = D3DXVECTOR3(20.0f, 20.0f, 20.0f);		//当たり判定
+}
+
+//====================================================================
+//コンストラクタ
+//====================================================================
+CRail::CRail(int nPriority) : CObjectX(nPriority)
+{
+	SetSize(SAMPLE_SIZE);
+	SetPos(INITVECTOR3);
+	m_nIdxXModel = 0;			//マテリアルの数
+	m_CollisionPos = INITVECTOR3;
+	m_bCollision = false;
+	m_State = STATE_NORMAL;
+	m_nStateCount = 0;
+	m_Scaling = 1.0f;
+	m_fColorA = 0.0f;
+}
+
+//====================================================================
+//デストラクタ
+//====================================================================
+CRail::~CRail()
+{
+
+}
+
+//====================================================================
+//生成処理
+//====================================================================
+CRail* CRail::Create(char* pModelName)
+{
+	CRail* pSample = nullptr;
+
+	if (pSample == nullptr)
+	{
+		//オブジェクト2Dの生成
+		pSample = new CRail();
+	}
+
+	//オブジェクトの初期化処理
+	if (FAILED(pSample->Init(pModelName)))
+	{//初期化処理が失敗した場合
+		return nullptr;
+	}
+
+	return pSample;
+}
+
+//====================================================================
+//初期化処理
+//====================================================================
+HRESULT CRail::Init(char* pModelName)
+{
+	SetType(CObject::TYPE_ENEMY3D);
+
+	CObjectX::Init(pModelName);
+
+	//モードごとに初期値を設定出来る
+	switch (CScene::GetMode())
+	{
+	case CScene::MODE_TITLE:
+		break;
+
+	case CScene::MODE_GAME:
+	case CScene::MODE_TUTORIAL:
+
+		break;
+
+	case CScene::MODE_RESULT:
+		break;
+	}
+
+
+	return S_OK;
+}
+
+//====================================================================
+//終了処理
+//====================================================================
+void CRail::Uninit(void)
+{
+	CObjectX::Uninit();
+}
+
+//====================================================================
+//更新処理
+//====================================================================
+void CRail::Update(void)
+{
+	switch (CScene::GetMode())
+	{
+	case CScene::MODE_TITLE:
+		TitleUpdate();
+		break;
+
+	case CScene::MODE_GAME:
+	case CScene::MODE_TUTORIAL:
+
+		GameUpdate();
+		break;
+
+	case CScene::MODE_RESULT:
+		break;
+	}
+}
+
+//====================================================================
+//タイトルでの更新処理
+//====================================================================
+void CRail::TitleUpdate(void)
+{
+	D3DXVECTOR3 pos = GetPos();
+
+	//位置更新
+	pos += m_move;
+
+	SetPos(pos);
+
+	//頂点情報の更新
+	CObjectX::Update();
+}
+
+//====================================================================
+//ゲームでの更新処理
+//====================================================================
+void CRail::GameUpdate(void)
+{
+	//更新前の位置を過去の位置とする
+	m_posOld = m_pos;
+
+	//位置更新
+	CObjectX::SetPos(m_pos);
+	CObjectX::SetRot(m_rot);
+
+	//画面外判定
+	if (m_pos.y < 0.0f)
+	{
+
+	}
+
+	//大きさの設定
+	SetScaling(D3DXVECTOR3(m_Scaling, m_Scaling, m_Scaling));
+
+	//状態管理
+	StateManager();
+
+	//頂点情報の更新
+	CObjectX::Update();
+}
+
+//====================================================================
+//描画処理
+//====================================================================
+void CRail::Draw(void)
+{
+	CObjectX::Draw();
+}
+
+//====================================================================
+//状態管理
+//====================================================================
+void CRail::StateManager(void)
+{
+	switch (m_State)
+	{
+	case STATE_NORMAL:
+		break;
+	case STATE_ACTION:
+		break;
+	}
+
+	if (m_nStateCount > 0)
+	{
+		m_nStateCount--;
+	}
+}
