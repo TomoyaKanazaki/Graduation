ec6492d8041d1a1b8ed42ee95a97ed4476c1ba57
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2024-10-29 11:16:58 +0900

    ビルボードの初期化終了にリスト処理追加

--- 00_base_Sakamoto/source/objectBillboard.cpp
+++ 00_base_Sakamoto/source/objectBillboard.cpp
@@ -117,6 +117,15 @@ HRESULT CObjectBillboard::Init(void)
 	//頂点バッファをアンロックする
 	m_pVtxBuff->Unlock();
 
+	if (m_pList == nullptr)
+	{// リストマネージャー生成
+		m_pList = CListManager<CObjectBillboard>::Create();
+		if (m_pList == nullptr) { assert(false); return E_FAIL; }
+	}
+
+	// リストに自身のオブジェクトを追加・イテレーターを取得
+	m_iterator = m_pList->AddList(this);
+
 	return S_OK;
 }
 
@@ -125,6 +134,16 @@ HRESULT CObjectBillboard::Init(void)
 //====================================================================
 void CObjectBillboard::Uninit(void)
 {
+	// リストから自身のオブジェクトを削除
+	m_pList->DelList(m_iterator);
+
+	if (m_pList->GetNumAll() == 0)
+	{ // オブジェクトが一つもない場合
+
+		// リストマネージャーの破棄
+		m_pList->Release(m_pList);
+	}
+
 	SetDeathFlag(true);
 }
 
