ab5acdd4a55a08cd304afa890e5ee3c8d3e3e8c3
sakai_minato <221095@st.yoshida-g.ac.jp>
2024-11-11 10:20:43 +0900

    キャラクタークラスの実装・傾き装置で仮実装・プルしないと動かないから動作未確認

--- 00_base_Sakamoto/source/character.cpp
+++ 00_base_Sakamoto/source/character.cpp
@@ -10,12 +10,22 @@
 #include "manager.h"
 #include "renderer.h"
 
+#include "model.h"
+#include "motion.h"
+
 //====================================================================
 //コンストラクタ
 //====================================================================
 CCharacter::CCharacter()
 {
+	for (int nCnt = 0; nCnt < MODEL_NUM; nCnt++)
+	{
+		m_apModel[nCnt] = nullptr;
+		m_aModelName[nCnt] = {};
+	}
 
+	m_pMotion = nullptr;
+	m_nNumModel = 0;
 }
 
 //====================================================================
@@ -47,6 +57,21 @@ CCharacter* CCharacter::Create(const char* pModelName)
 //====================================================================
 HRESULT CCharacter::Init(const char* pModelName)
 {
+	strcpy(&m_aModelName[0], pModelName);
+
+	//モデルの生成
+	LoadModel(pModelName);
+
+	//モーションの生成
+	if (m_pMotion == nullptr)
+	{
+		//モーションの生成
+		m_pMotion = new CMotion;
+	}
+
+	//初期化処理
+	m_pMotion->SetModel(&m_apModel[0], m_nNumModel);
+	m_pMotion->LoadData(pModelName);
 
 	return S_OK;
 }
@@ -56,7 +81,20 @@ HRESULT CCharacter::Init(const char* pModelName)
 //====================================================================
 void CCharacter::Uninit(void)
 {
+	for (int nCntModel = 0; nCntModel < m_nNumModel; nCntModel++)
+	{
+		m_apModel[nCntModel]->Uninit();
+		delete m_apModel[nCntModel];
+		m_apModel[nCntModel] = nullptr;
+	}
 
+	//モーションの終了処理
+	if (m_pMotion != nullptr)
+	{
+		//モーションの破棄
+		delete m_pMotion;
+		m_pMotion = nullptr;
+	}
 }
 
 //====================================================================
@@ -64,7 +102,11 @@ void CCharacter::Uninit(void)
 //====================================================================
 void CCharacter::Update(void)
 {
-
+	if (m_pMotion != nullptr)
+	{
+		//モーションの更新
+		m_pMotion->Update();
+	}
 }
 
 //====================================================================
@@ -72,5 +114,145 @@ void CCharacter::Update(void)
 //====================================================================
 void CCharacter::Draw(void)
 {
+	//モデルの描画(全パーツ)
+	for (int nCntModel = 0; nCntModel < m_nNumModel; nCntModel++)
+	{
+		m_apModel[nCntModel]->Draw();
+	}
+}
 
+//====================================================================
+// モデルロード処理
+//====================================================================
+void CCharacter::LoadModel(const char* pFilename)
+{
+	//ファイルを開く
+	FILE* pFile = fopen(pFilename, "r");
+
+	if (pFile != nullptr)
+	{//ファイルが開けた場合
+		int ModelParent = 0;
+		D3DXVECTOR3 ModelPos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		D3DXVECTOR3 ModelRot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+		char ModelName[128] = {};
+		int nCntModel = 0;
+		int nCntParts = 0;
+		int nCntMotion = 0;
+		int nCntKeySet = 0;
+		int nCntKey = 0;
+
+		char aString[128] = {};				//ゴミ箱
+		char aMessage[128] = {};			//スタートとエンドのメッセージ
+		char aBool[128] = {};				//bool変換用メッセージ
+
+		// 読み込み開始-----------------------------------------------------
+		while (1)
+		{//「SCRIPT」を探す
+			fscanf(pFile, "%s", &aMessage[0]);
+			if (strcmp(&aMessage[0], "SCRIPT") == 0)
+			{
+				// モデル数読み込み-----------------------------------------------------
+				while (1)
+				{//「NUM_MODEL」を探す
+					fscanf(pFile, "%s", &aMessage[0]);
+					if (strcmp(&aMessage[0], "NUM_MODEL") == 0)
+					{
+						fscanf(pFile, "%s", &aString[0]);
+						fscanf(pFile, "%d", &m_nNumModel);		//モデル数の設定
+						break;
+					}
+				}
+
+				//モデルファイルの読み込み
+				while (1)
+				{//「MODEL_FILENAME」を探す
+					fscanf(pFile, "%s", &aMessage[0]);
+					if (strcmp(&aMessage[0], "MODEL_FILENAME") == 0)
+					{
+						fscanf(pFile, "%s", &aString[0]);
+						fscanf(pFile, "%s", &ModelName[0]);		//読み込むモデルのパスを取得
+
+						m_apModel[nCntModel] = CModel::Create(&ModelName[0]);
+						m_apModel[nCntModel]->SetColorType(CModel::COLORTYPE_FALSE);
+						nCntModel++;
+					}
+					if (nCntModel >= m_nNumModel)
+					{
+						nCntModel = 0;
+						break;
+					}
+				}
+
+				// キャラクター情報読み込み-----------------------------------------------------
+				while (1)
+				{//「PARTSSET」を探す
+					fscanf(pFile, "%s", &aMessage[0]);
+					if (strcmp(&aMessage[0], "PARTSSET") == 0)
+					{
+						while (1)
+						{//各種変数を探す
+							fscanf(pFile, "%s", &aMessage[0]);
+							if (strcmp(&aMessage[0], "INDEX") == 0)
+							{
+								fscanf(pFile, "%s", &aString[0]);
+								fscanf(pFile, "%d", &nCntModel);	//インデックスを設定
+							}
+							if (strcmp(&aMessage[0], "PARENT") == 0)
+							{
+								fscanf(pFile, "%s", &aString[0]);
+								fscanf(pFile, "%d", &ModelParent);	//親モデルのインデックスを設定
+
+								if (ModelParent == -1)
+								{
+									m_apModel[nCntModel]->SetParent(nullptr);
+								}
+								else
+								{
+									m_apModel[nCntModel]->SetParent(m_apModel[ModelParent]);
+								}
+							}
+							if (strcmp(&aMessage[0], "POS") == 0)
+							{
+								fscanf(pFile, "%s", &aString[0]);
+								fscanf(pFile, "%f", &ModelPos.x);				//位置(オフセット)の初期設定
+								fscanf(pFile, "%f", &ModelPos.y);				//位置(オフセット)の初期設定
+								fscanf(pFile, "%f", &ModelPos.z);				//位置(オフセット)の初期設定
+
+								m_apModel[nCntModel]->SetPos(ModelPos);
+								m_apModel[nCntModel]->SetStartPos(ModelPos);
+							}
+							if (strcmp(&aMessage[0], "ROT") == 0)
+							{
+								fscanf(pFile, "%s", &aString[0]);
+								fscanf(pFile, "%f", &ModelRot.x);				////向きの初期設定
+								fscanf(pFile, "%f", &ModelRot.y);				////向きの初期設定
+								fscanf(pFile, "%f", &ModelRot.z);				////向きの初期設定
+
+								m_apModel[nCntModel]->SetRot(ModelRot);
+								m_apModel[nCntModel]->SetStartRot(ModelRot);
+							}
+							if (strcmp(&aMessage[0], "END_PARTSSET") == 0)
+							{
+								break;
+							}
+						}
+						nCntModel++;
+						if (nCntModel >= m_nNumModel)
+						{
+							break;
+						}
+					}
+				}
+			}
+			if (strcmp(&aMessage[0], "END_SCRIPT") == 0)
+			{
+				break;
+			}
+		}
+		fclose(pFile);
+	}
+	else
+	{//ファイルが開けなかった場合
+		printf("***ファイルを開けませんでした***\n");
+	}
 }
