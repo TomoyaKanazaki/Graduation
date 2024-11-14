216ca9d611fa79ee60dad0c639267fede42a662d
TomoyaKanazaki <221083@st.yoshida-g.ac.jp>
2024-11-14 14:04:33 +0900

    AStarの最適化中

--- 00_base_Sakamoto/source/enemy.cpp
+++ 00_base_Sakamoto/source/enemy.cpp
@@ -48,10 +48,20 @@ namespace
 	const float DISTANCE_RECEDE = 200.0f;	//近づく距離
 	const float DISTANCE_APPROACH = 100.0f;	//遠ざかる距離
 
-	const float COORDDINATE_RATE = 5.0f; // 経路探索を行う間隔
+	const float COORDDINATE_RATE[] = // 経路探索を行う間隔
+	{
+		5.0f,
+		3.0f,
+		1.0f
+	};
 	const float TARGET_DIFF = 5.0f; // 許容範囲
-	const float MOVE_ASTAR = 50.0f; // 追跡時の移動速度
+	const float MOVE_ASTAR = 150.0f; // 追跡時の移動速度
 }
+
+//==========================================
+//  静的警告処理
+//==========================================
+static_assert(NUM_ARRAY(COORDDINATE_RATE) == CEnemy::ENEMY_MAX, "ERROR : Type Count Missmatch");
 
 //===========================================
 // 静的メンバ変数宣言
@@ -739,13 +749,13 @@ void CEnemy::Coordinate()
 	m_fCoordinateTimer += DeltaTime::Get();
 
 	// 探索のタイミングでない場合関数を抜ける
-	if (m_fCoordinateTimer < COORDDINATE_RATE) { return; }
+	if (m_fCoordinateTimer < COORDDINATE_RATE[m_EnemyType]) { return; }
 
 	// 最短経路の次の目標をリセット
-	m_nTargetIndex = 0;
+	m_nTargetIndex = 1;
 
 	// タイマーのリセット
-	m_fCoordinateTimer -= COORDDINATE_RATE;
+	m_fCoordinateTimer -= COORDDINATE_RATE[m_EnemyType];
 
 	// 最短経路を取得
 	AStar::CoordinateList Path = AStar::Generator::GetInstance()->FindPlayer({ m_Grid.x, m_Grid.z });
@@ -780,18 +790,13 @@ void CEnemy::Route()
 		return;
 	}
 
-	for (int i = 0; i < m_nNumCoordinate; ++i)
-	{
-		DebugProc::Print(DebugProc::POINT_CENTER, "最短グリッド [ %d ] : %d, %d\n", i, m_pPath[i].x, m_pPath[i].z);
-	}
-
 	// 目標地点の座標を求める
 	D3DXVECTOR3 path = CMapSystem::GetInstance()->GetGritPos(m_pPath[m_nTargetIndex]);
 
 	// 次に向かうグリッドに重なったらその次の目標を設定
 	if (fabsf(path.x - pos.x) <= TARGET_DIFF &&
 		fabsf(path.z - pos.z) <= TARGET_DIFF) // 一定範囲内であれば
-	{/* 座標の比較 */
+	{
 		// インデックス番号を加算
 		m_nTargetIndex++;
 		path = CMapSystem::GetInstance()->GetGritPos(m_pPath[m_nTargetIndex]);
@@ -799,18 +804,17 @@ void CEnemy::Route()
 
 	// 次の目標が存在しなかったら関数を抜ける
 	if (m_nTargetIndex >= m_nNumCoordinate)
-	{ 
+	{
 		return;
 	}
-
-	DebugProc::Print(DebugProc::POINT_CENTER, "目標地点 [ %d ] : %d, %d\n", m_nTargetIndex, m_pPath[m_nTargetIndex].x, m_pPath[m_nTargetIndex].z);
-	DebugProc::Print(DebugProc::POINT_CENTER, "目標地点 [ %d ] : %f, %f\n", m_nTargetIndex, path.x, path.z);
 
 	// 次の目標位置との角度
 	float RotDest = atan2f(path.z - pos.z, path.x - pos.x);
 
 	// 次の目標位置に移動
-	move = (path - pos) * DeltaTime::Get();
+	move = path - pos;
+	D3DXVec3Normalize(&move, &move);
+	move *= DeltaTime::Get() * MOVE_ASTAR;
 
 	// 位置更新
 	pos += move;
