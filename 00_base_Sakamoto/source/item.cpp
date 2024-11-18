523d48b24954ba739cb597d292c3bafe35a2ac5d
金崎朋弥 <221083@st.yoshida-g.ac.jp>
2024-11-18 12:07:20 +0900

    アイテム配置バグ修正

--- 00_base_Sakamoto/source/item.cpp
+++ 00_base_Sakamoto/source/item.cpp
@@ -176,11 +176,6 @@ void CItem::Update()
 	SetRot(rot);
 	m_Grid = CMapSystem::GetInstance()->CalcGrid(pos);
 
-	// アイテムの種類を表示してみる
-	//auto str = magic_enum::enum_name(m_eType);
-	//DebugProc::Print(DebugProc::POINT_CENTER, str.data());
-	//DebugProc::Print(DebugProc::POINT_CENTER, "\n");
-
 	// 親クラスの更新処理
 	CObjectX::Update();
 }
@@ -222,6 +217,9 @@ void CItem::Draw()
 //==========================================
 void CItem::SetGrid(const CMapSystem::GRID& pos)
 {
+	// グリッド情報を保存する
+	m_Grid = pos;
+
 	// グリッド情報から自身の座標を算出する
 	m_posBase = CMapSystem::GetInstance()->GetGritPos(pos);
 
