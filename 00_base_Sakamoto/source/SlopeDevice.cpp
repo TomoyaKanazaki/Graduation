98a32b1395d6387b5dce5e482dec802600ea2b09
sakai_minato <221095@st.yoshida-g.ac.jp>
2024-12-02 11:32:46 +0900

    傾き装置のテスト動作【上昇・下降】（繰り返すだけのテスト）

--- 00_base_Sakamoto/source/SlopeDevice.cpp
+++ 00_base_Sakamoto/source/SlopeDevice.cpp
@@ -23,7 +23,8 @@
 //==========================================
 namespace
 {
-	const D3DXVECTOR3 ROTATE_ADD = D3DXVECTOR3(0.0f, 0.0f, 0.05f);
+	const D3DXVECTOR3 ASCENT_ADD = D3DXVECTOR3(0.0f, 1.5f, 0.0f);	// 上昇量
+	const D3DXVECTOR3 DESCENT_DEST = D3DXVECTOR3(0.0f, 1.5f, 0.0f);	// 下降量
 }
 
 //====================================================================
@@ -36,7 +37,7 @@ CListManager<CSlopeDevice>* CSlopeDevice::m_pList = nullptr; // オブジェクトリス
 //====================================================================
 CSlopeDevice::CSlopeDevice(int nPriority) : CObjectCharacter(nPriority)
 {
-	m_State = STATE(0);
+	m_State = STATE(1);
 	m_nStateCount = 0;
 }
 
@@ -139,7 +140,7 @@ void CSlopeDevice::Update(void)
 	posOldMy = posMy;
 
 	//状態管理
-	StateManager(rotMy);
+	StateManager();
 
 	// キャラクタークラスの更新（継承）
 	CObjectCharacter::Update();
@@ -173,13 +174,20 @@ HRESULT CSlopeDevice::InitModel(const char* pModelNameSlopeDevice, const char* p
 //====================================================================
 //状態管理
 //====================================================================
-void CSlopeDevice::StateManager(D3DXVECTOR3& rotMy)
+void CSlopeDevice::StateManager(void)
 {
 	switch (m_State)
 	{
 	case STATE_NORMAL:
 		break;
-	case STATE_ROTATE:
+	case STATE_ASCENT:
+
+		Ascent(SETUP_TYPE_ELEVATING_PART, ASCENT_ADD,D3DXVECTOR3(0.0f,200.0f,0.0f));
+
+		break;
+	case STATE_DESCENT:
+
+		Descent(SETUP_TYPE_ELEVATING_PART, DESCENT_DEST, D3DXVECTOR3(0.0f, 80.0f, 0.0f));
 
 		break;
 	}
@@ -189,6 +197,69 @@ void CSlopeDevice::StateManager(D3DXVECTOR3& rotMy)
 		m_nStateCount--;
 	}
 }
+
+//====================================================================
+// 上昇処理
+//====================================================================
+void CSlopeDevice::Ascent(int nNldxModel, D3DXVECTOR3 ascent, D3DXVECTOR3 ascentPosMax)
+{
+	// モデルの取得
+	CModel* pModel = GetModel(nNldxModel);
+
+	if (pModel == nullptr)
+	{
+		return;
+	}
+
+	// モデルの位置を取得
+	D3DXVECTOR3 pos = pModel->GetStartPos();
+
+	// 最大上昇位置判定
+	if (pos.y <= ascentPosMax.y)
+	{
+		// 上昇量を加算
+		pos += ascent;
+	}
+	else
+	{
+		m_State = STATE(2);
+	}
+
+	// モデルの位置更新
+	pModel->SetStartPos(pos);
+}
+
+//====================================================================
+// 下降処理
+//====================================================================
+void CSlopeDevice::Descent(int nNldxModel, D3DXVECTOR3 descent, D3DXVECTOR3 descentPosMin)
+{
+	// モデルの取得
+	CModel* pModel = GetModel(nNldxModel);
+
+	if (pModel == nullptr)
+	{
+		return;
+	}
+
+	// モデルの位置を取得
+	D3DXVECTOR3 pos = pModel->GetStartPos();
+
+	// 最大下降位置判定
+	if (pos.y >= descentPosMin.y)
+	{
+		// 下降量を減算
+		pos -= descent;
+	}
+	else
+	{
+		m_State = STATE(1);
+	}
+
+	// モデルの位置更新
+	pModel->SetStartPos(pos);
+}
+
 
 //====================================================================
 //リスト取得
