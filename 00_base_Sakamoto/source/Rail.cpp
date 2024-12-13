c7ead08cad4bb6cefdbabe86509a3abe7c0a2950
ShionSatone <221081@st.yoshida-g.ac.jp>
2024-12-13 09:25:09 +0900

    レールの向きががNONEの時にレールを生成しないようにした

--- 00_base_Sakamoto/source/Rail.cpp
+++ 00_base_Sakamoto/source/Rail.cpp
@@ -101,41 +101,46 @@ HRESULT CRail::Init()
 	// レールモデル
 	for (int nCnt = 0; nCnt < POSSTATE_MAX; nCnt++)
 	{
-		m_pRailModel[nCnt] = CObjectX::Create(FILE_PASS);	// 生成処理
-		m_pRailModel[nCnt]->SetGrid(m_Grid);				// グリッド設定
-		m_pRailModel[nCnt]->SetPos(pos);					// 位置設定
-		m_pRailModel[nCnt]->SetPosOld(pos);					// 前回の位置設定
-		m_pRailModel[nCnt]->SetType(CObject::TYPE_RAIL);
+		if (m_PosType[nCnt] == POSTYPE_NONE) { continue; }		// レールに向きが無い場合生成しない
+		else if (m_PosType[nCnt] != POSTYPE_NONE)
+		{ // レールに向きがある場合
 
-		//マップとのマトリックスの掛け合わせをオンにする
-		m_pRailModel[nCnt]->SetUseMultiMatrix(CObjmeshField::GetListTop()->GetMatrix());
+			m_pRailModel[nCnt] = CObjectX::Create(FILE_PASS);	// 生成処理
+			m_pRailModel[nCnt]->SetGrid(m_Grid);				// グリッド設定
+			m_pRailModel[nCnt]->SetPos(pos);					// 位置設定
+			m_pRailModel[nCnt]->SetPosOld(pos);					// 前回の位置設定
+			m_pRailModel[nCnt]->SetType(CObject::TYPE_RAIL);
 
-		// 設置する向き
-		switch (m_PosType[nCnt])
-		{
-		case CRail::POSTYPE_UP:		// 上
-			rot.y = D3DX_PI * 1.0f;
-			break;
+			//マップとのマトリックスの掛け合わせをオンにする
+			m_pRailModel[nCnt]->SetUseMultiMatrix(CObjmeshField::GetListTop()->GetMatrix());
 
-		case CRail::POSTYPE_DOWN:	// 下
-			rot.y = D3DX_PI * 0.0f;
-			break;
+			// 設置する向き
+			switch (m_PosType[nCnt])
+			{
+			case CRail::POSTYPE_UP:		// 上
+				rot.y = D3DX_PI * 1.0f;
+				break;
 
-		case CRail::POSTYPE_LEFT:	// 左
-			rot.y = D3DX_PI * 0.5f;
-			break;
+			case CRail::POSTYPE_DOWN:	// 下
+				rot.y = D3DX_PI * 0.0f;
+				break;
 
-		case CRail::POSTYPE_RIGHT:	// 右
-			rot.y = D3DX_PI * -0.5f;
-			break;
+			case CRail::POSTYPE_LEFT:	// 左
+				rot.y = D3DX_PI * 0.5f;
+				break;
 
-		default:
-			break;
+			case CRail::POSTYPE_RIGHT:	// 右
+				rot.y = D3DX_PI * -0.5f;
+				break;
+
+			default:
+				break;
+			}
+
+			// 向き設定
+			m_pRailModel[nCnt]->SetRot(rot);
+			rot = INITVECTOR3;
 		}
-
-		// 向き設定
-		m_pRailModel[nCnt]->SetRot(rot);
-		rot = INITVECTOR3;
 	}
 
 	SetType(CObject::TYPE_RAIL);
